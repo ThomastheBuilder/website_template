f2cabcb869d1539aed5bfd88209f0571b8c89dc1
ThomastheBuilder <91571855+ThomastheBuilder@users.noreply.github.com>
2022-12-31 09:49:16 -0500

    Changed background to solid fill. Removed about page and moved content to home. Added social media icons.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,21 +7,19 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { HeaderComponent } from './header/header.component';
-import { AboutComponent } from './about/about.component';
 import { DonateComponent } from './donate/donate.component';
 import { EventsComponent } from './events/events.component';
 import { IssuesComponent } from './issues/issues.component';
 import { ContactComponent } from './contact/contact.component';
 import { FooterComponent } from './footer/footer.component';
 
-const routes: Routes = [ {path: 'about', component: AboutComponent}, {path: 'contact', component: ContactComponent}, {path: 'donate', component: DonateComponent}, {path: 'events', component: EventsComponent}, {path: 'issues', component: IssuesComponent}, {path: 'home', component: HomeComponent}]
+const routes: Routes = [ {path: 'contact', component: ContactComponent}, {path: 'donate', component: DonateComponent}, {path: 'events', component: EventsComponent}, {path: 'issues', component: IssuesComponent}, {path: 'home', component: HomeComponent}]
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     HeaderComponent,
-    AboutComponent,
     DonateComponent,
     EventsComponent,
     IssuesComponent,
@@ -38,4 +36,4 @@ const routes: Routes = [ {path: 'about', component: AboutComponent}, {path: 'con
   bootstrap: [AppComponent]
 })
 export class AppModule { }
-export const routingComponents = [AboutComponent, HomeComponent, DonateComponent, EventsComponent, IssuesComponent, ContactComponent]
+export const routingComponents = [HomeComponent, DonateComponent, EventsComponent, IssuesComponent, ContactComponent]
