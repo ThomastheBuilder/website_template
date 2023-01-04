a7f3f1d354ad409ef5d3a0dabf314670ee615de7
ThomastheBuilder <91571855+ThomastheBuilder@users.noreply.github.com>
2023-01-04 09:25:29 -0500

    Added labels to most code pages. Added Google Analytics.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { RouterModule, Routes } from '@angular/router';
-
+import { NgxGoogleAnalyticsModule, NgxGoogleAnalyticsRouterModule } from 'ngx-google-analytics';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
@@ -31,6 +31,8 @@ const routes: Routes = [ {path: 'contact', component: ContactComponent}, {path: 
     AppRoutingModule,
     RouterModule.forRoot(routes),
     FormsModule,
+    NgxGoogleAnalyticsModule.forRoot('G-L02Q5QQTWS'),
+    NgxGoogleAnalyticsRouterModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
